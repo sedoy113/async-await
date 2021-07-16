07071941a979a8c058d3ba7646d91b6a37285a97
sedoy113 <sedoy113@yandex.ru>
2021-07-16 19:53:34 +0300

    all good

---
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -0,0 +1,7 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+test('works with async/await', async () => {
+  expect.assertions(1);
+  const data = await GameSavingLoader.load();
+  expect(data).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
+});
---
+++ src/js/parser.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function json(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
+    }, 500);
+  });
+}
